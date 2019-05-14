d08dc8d1e1c3cb5b5814572829ca3cd1b38bc050
vozoul <vozoul@gmail.com>
2019-05-14 17:50:21 +0200

    last

--- src/Interface/Interface.java
+++ src/Interface/Interface.java
@@ -3,6 +3,7 @@ package Interface;
 import Perso.Classes.Magic;
 import Perso.Classes.Warrior;
 import Perso.Perso;
+
 import java.util.Scanner;
 
 public class Interface {
@@ -17,28 +18,29 @@ public class Interface {
     private int id = 0;
 
     //Demarrage
-    public void start(){
+    public void start() {
         System.out.println("Bonjour jeune paladin !\nSouhaite tu construire une armée\nafin d'occire quelques dragon\n");
         System.out.println("(O)ui / (N)on ?");
         play = scan.nextLine().charAt(0);
-        while(play == 'O'){
+        while (play == 'O') {
             menu();
-            if(menuResponse == 'C') {
+            if (menuResponse == 'C') {
                 createPlayer();
-            }else if(menuResponse == 'M') {
+            } else if (menuResponse == 'M') {
                 selectCombatant("modif");
-            }else if(menuResponse == 'S') {
+            } else if (menuResponse == 'S') {
                 selectCombatant("select");
-            }else if(menuResponse == 'A') {
+            } else if (menuResponse == 'A') {
                 affiche();
-            }else{
+            } else {
                 play = 'N';
             }
         }
         System.out.println("Au revoir jeune(s) menestrel(s)");
     }
+
     //menu principale
-    public void menu(){
+    public void menu() {
         System.out.println("Quelle actions voulez vous faire ?\n" +
                 " -(C)réer un combatant \n" +
                 " -(A)fficher les combatant \n" +
@@ -49,32 +51,35 @@ public class Interface {
     }
 
     //creation d'un Perso
-    public void createPlayer(){
+    public void createPlayer() {
         System.out.println("Construit ton armée");
+
         personnage = setClasse(personnage);
+
         personnage.setId(id);
         System.out.println("Quel nom voulez vous ?");
         personnage.setName(scan.nextLine());
-        setVie(personnage);
-        setForce(personnage);
+        params(personnage, "vie");
+        params(personnage, "force");
         persos[id] = personnage;
         id++;
     }
 
     //liste et choix de personnage
-    public void affiche(){
-        if(isCombatant()) {
+    public void affiche() {
+        if (isCombatant()) {
             for (int i = 0; i < persos.length; i++) {
                 if (persos[i] != null) {
                     System.out.println(persos[i]);
                 }
             }
-        }else{
+        } else {
             noCombatant();
         }
     }
-    public void selectCombatant(String action){
-        if(isCombatant()) {
+
+    public void selectCombatant(String action) {
+        if (isCombatant()) {
             int exitMenu = 0;
             System.out.println("Choisissez un personnage");
             for (int i = 0; i < persos.length; i++) {
@@ -85,134 +90,133 @@ public class Interface {
             }
             System.out.println(exitMenu + " - Fin du choix");
             int choice = scan.nextInt();
-            if(action == "modif") {
+            if (action == "modif") {
                 if (choice != exitMenu || choice < exitMenu) {
                     selectModif(choice);
                 }
-            }else if (action == "select"){
+            } else if (action == "select") {
                 currentPlayer = persos[choice];
             }
             scan.nextLine();
-        }else{
+        } else {
             noCombatant();
         }
     }
 
     //edit & modifie un perso
-    public void selectModif(int id){
+    public void selectModif(int id) {
         Perso perso = persos[id];
         System.out.println("quelle modification voulez vous apporter");
         System.out.println(" 1 - Reset Name \n 2 - Reset Vie \n 3 - Reset Attaque \n 4 - Supprimer le personnage \n 5 - Fin de modife");
         int choice = scan.nextInt();
         scan.nextLine();
-        if(choice == 1){
+        if (choice == 1) {
             resetName(id);
-        }else if(choice == 2){
-            setVie(perso);
-        }else if(choice == 3){
-            setForce(perso);
-        }else if(choice == 4){
+        } else if (choice == 2) {
+            params(perso, "vie");
+        } else if (choice == 3) {
+            params(perso, "force");
+        } else if (choice == 4) {
             delete(id);
         }
     }
-    public Perso setClasse(Perso hero){
+
+    public Perso setClasse(Perso hero) {
         char trooper;
         System.out.println("(G)uerrier / (M)agicien ?");
         trooper = scan.nextLine().charAt(0);
         if (trooper == 'G') {
             hero = new Warrior();
+            int min_vie = 5;
+            int max_vie = 10;
+            int min_force = 5;
+            int max_force = 10;
             hero.setType("Guerrier");
         } else if (trooper == 'M') {
             hero = new Magic();
+            int min_vie = 3;
+            int max_vie = 6;
+            int min_force = 8;
+            int max_force = 15;
             hero.setType("Magicien");
         }
         return hero;
     }
-    public void resetName(int id){
+
+    public void resetName(int id) {
         Perso perso = persos[id];
         String oldName = perso.getName();
         System.out.println("modifié le nom ? (pas de changement si null");
         String newName = scan.nextLine();
-        if(newName.isEmpty()){
+        if (newName.isEmpty()) {
             perso.setName(oldName);
-        }else{
+        } else {
             perso.setName(newName);
         }
     }
-    public void setVie(Perso personnage){
+
+    public void params(Perso personnage, String type){
         boolean checked = false;
-        int value = 0;
-        while(checked == false) {
-            System.out.println("Repartissez vos XP, Vos point de vie entre " + personnage.getMin_vie() + " et " + personnage.getMax_vie());
+        int min = 0;
+        int max = 0;
+        while (checked == false) {
+            if(type == "vie"){
+                min = personnage.getMin_vie();
+                max = personnage.getMax_vie();
+            }else if(type == "force"){
+                min = personnage.getMin_force();
+                max = personnage.getMax_force();
+            }
+            System.out.println("Repartissez vos XP, Vos point de "+type+" entre " + min+ " et " + max);
             int val = scan.nextInt();
             scan.nextLine();
-            checked = check(val, "vie");
-            value = val;
-            if(checked) {
-                personnage.setLife(value);
+            checked = check(val, min, max);
+            if (checked && type == "vie") {
+                    personnage.setLife(val);
+            }else if(checked && type == "force"){
+                    personnage.setAtk(val);
             }
         }
     }
-    public void setForce(Perso personnage){
-        boolean checked = false;
-        int value = 0;
-        while(checked == false) {
-            System.out.println("Repartissez vos XP, Vos point d'attaque entre " + personnage.getMin_force() + " et " + personnage.getMax_force());
-            int val = scan.nextInt();
-            scan.nextLine();
-            checked = check(val, "atk");
-            value = val;
-            if(checked) {
-                personnage.setAtk(value);
-            }
-        }
-    }
-    public void delete(int id){
+
+    public void delete(int id) {
         System.out.println("Etesvous sur de vouloir supprimer ? (O)ui / (N)on");
         char response = scan.nextLine().charAt(0);
-        if(response == 'O'){
+        if (response == 'O') {
             persos[id] = null;
             System.out.println("Suppression OK");
-        }else{
+        } else {
             System.out.println("Suppression Annuler");
         }
     }
 
     //check si entre val max et val min
-    public boolean check(int val, String type){
-        int min = 0;
-        int max = 0;
-        if(type == "vie"){
-            min = personnage.getMin_vie();
-            max = personnage.getMax_vie();
-        }else if(type == "atk"){
-            min = personnage.getMin_force();
-            max = personnage.getMax_force();
-        }
-        if(val >= min && val <= max) {
+    public boolean check(int val, int min, int max) {
+        if (val >= min && val <= max) {
             return true;
         }
         return false;
     }
 
     //verifie si des combatants
-    public boolean isCombatant(){
-        int combatant=0;
-        for(int i =0; i<persos.length; i++){
-            if(persos[i] != null) {
+    public boolean isCombatant() {
+        int combatant = 0;
+        for (int i = 0; i < persos.length; i++) {
+            if (persos[i] != null) {
                 combatant++;
             }
         }
-        if(combatant>0){
+        if (combatant > 0) {
             return true;
         }
         return false;
     }
-    public void noCombatant(){
-        System.out.println("\n"+
-                "/=====**************************=====\\"+"\n\n"+
-                "     Vous n'avez pas de combatant     "+"\n\n"+
-                "\\=====**************************=====/"+"\n\n"
+
+    public void noCombatant() {
+        System.out.println("\n" +
+                "/=====**************************=====\\" + "\n\n" +
+                "     Vous n'avez pas de combatant     " + "\n\n" +
+                "\\=====**************************=====/" + "\n\n"
         );
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 import Interface.Interface;
+import java.awt.*;
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+//        JFrame appli = new JFrame();
+//        appli.setTitle("Donjon & Dragon");
+//        appli.setSize(800,600);
+//        appli.setLocationRelativeTo(null);
+//        appli.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        appli.setVisible(true);
+//        JPanel jeu = new JPanel();
+//        jeu.setBackground(Color.ORANGE);
+//        appli.setContentPane(jeu);
         new Interface().start();
     }
 }
